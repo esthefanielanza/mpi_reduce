49f6ba5f53979db7e4d87b5d8626abac6126dc1d
Esthefanie Lanza <esthefanielanza@gmail.com>
2019-10-12 19:55:02 -0300

    Fixed prints

--- Makefile
+++ Makefile
@@ -2,25 +2,10 @@ CC = gcc
 CFLAGS = -g -O0 -Wall -Wextra -Werror -std=c99 -fopenmp
 LIBS = -lm
 
-all: sequential parallel clean
+all: parallel clean
 
-sequential: sequential.o input.o
-	$(CC) $(CFLAGS) sequential.o input.o -o sequential $(LIBS)
-
-sequential.o: sequential.c input.h
-	$(CC) $(CFLAGS) -c sequential.c
-
-parallel: parallel.o input.o pthreadPool.o
-	$(CC) $(CFLAGS) parallel.o input.o pthreadPool.o -o parallel $(LIBS)
-
-parallel.o: parallel.c input.h pthreadPool.h
-	$(CC) $(CFLAGS) -c parallel.c
-
-input.o: input.c input.h
-	$(CC) $(CFLAGS) -c input.c
-
-pthreadPool.o: pthreadPool.c pthreadPool.h
-	$(CC) $(CFLAGS) -c pthreadPool.c
+parallel: parallel.o
+	$(CC) $(CFLAGS) parallel.o -o parallel $(LIBS)
 
 clean:
 	rm *.o
--- parallel.c
+++ parallel.c
@@ -1,29 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<mpi.h>
 #include <time.h>
 #include <omp.h>
 
 int main (int argc, char *argv[]) {
   
-  int num_proccess = atoi(argv[0]); // numero de processos
+  int num_proccess = atoi(argv[1]); // numero de processos
+  printf("num_proccess: %d\n", num_proccess);
 
   char output[4]; // sum (somente soma), time (somente tempo), all
   fgets(output, sizeof(output), stdin);
+  printf("output: %s\n", output);
   
   int length = 0; // quantidade de números a serem somado
   scanf("%d", &length);
+  printf("length: %d\n", length);
 
   float *numbers; // numeros do tipo float a serem somados
   numbers = malloc(length * sizeof(float));
   for (int i = 0; i < length; i++) {
     scanf("%f", &numbers[i]);
   }
-
-  printf(num_proccess);
-  printf(output);
-  printf(length);
-  printf(numbers);
-
+  
+  printf("numbers\n");
+  for (int i = 0; i < length; i++) {
+    printf("%f ", numbers[i]);
+    
+  }
+  
+  (void)argc;
 	return 0;
 }
