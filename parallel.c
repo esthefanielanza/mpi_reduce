b9a4079b096fdc81c6f038e118ba9d5c5c2a7457
Esthefanie Lanza <esthefanielanza@gmail.com>
2019-10-13 19:19:19 -0300

    Read data and split array using mpi

--- parallel.c
+++ parallel.c
@@ -8,7 +8,7 @@
 
 int MAX_STRING = 100;
 
-void readMetadata(int nProcess, int myRank, char outputType[5], int *length, double *numbersPerProccess) {
+void readMetadata(int nProcess, int myRank, char outputType[5], int *length, float *numbersPerProccess) {
   if(myRank == 0) {
     int i;
 
@@ -16,47 +16,76 @@ void readMetadata(int nProcess, int myRank, char outputType[5], int *length, dou
     scanf("%d", length);
 
     *numbersPerProccess = ceil(*length / nProcess);
-    //Sending to the other process how many numbers they should handle 
+    // Sending to the other process how many numbers they should handle 
     for(i = 1; i < nProcess; i++) {
-      // printf("Sending %f to %d\n", *numbersPerProccess, i);
-      MPI_Send(numbersPerProccess, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+      // 
+      MPI_Send(numbersPerProccess, 1, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
     }
   } else {
-    MPI_Recv(numbersPerProccess, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(numbersPerProccess, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     // printf("thread %d receiving %f\n", myRank, *numbersPerProccess);
   }
 }
 
+void readArrayAndSplitData(int nProcess, int myRank, int length, float numbersPerProccess, float *partition) {
+  int i;
+  float currentNumber;
+
+  if(myRank == 0) {
+    int sendTo;
+
+    for(i = 0; i < length; i++) {
+      scanf("%f", &currentNumber);
+      sendTo = i % nProcess;
+
+      if(sendTo == 0) {
+        partition[i / nProcess] = currentNumber;
+      } else {
+        // address of sent buffer; number of elements in sent buffer; datetype; 
+        // rank of destination;message tag; communicator
+        MPI_Send(&currentNumber, 1, MPI_FLOAT, sendTo, 0, MPI_COMM_WORLD);
+      }
+    }
+  } else {    
+    for(i = 0; i < numbersPerProccess; i++) {
+      MPI_Recv(&currentNumber, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      partition[i] = currentNumber;
+    }
+  }
+}
+
+// sumOfElements(float *partition) {
+//   float sum = 0.0;
+
+  
+// }
+
 int main (void) {
   int nProcess, myRank, length;
-  double numbersPerProccess;
+  float numbersPerProccess;
   char outputType[5];
 
-  
-  // printf("output: %s\n", output);
-  
-
-  // printf("length: %d\n", length);
-
-  // float *numbers; // numeros do tipo float a serem somados
-  // numbers = malloc(length * sizeof(float));
-  // for (int i = 0; i < length; i++) {
-  //   scanf("%f", &numbers[i]);
-  // }
-  
-  // printf("numbers\n");
-  // for (int i = 0; i < length; i++) {
-  //   printf("%f ", numbers[i]);
-    
-  // }
-
+  // Initialize the MPI environment
   MPI_Init(NULL, NULL);
+  // Get the number of processes
   MPI_Comm_size(MPI_COMM_WORLD, &nProcess);
+  // Get the rank of the processes
   MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
+  // readMetadata: reads the input (stdin)
   readMetadata(nProcess, myRank, outputType, &length, &numbersPerProccess);
-  // readArrayAndSplitData();
+ 
+  // readArrayAndSplitData: read float array and splits between processes
+  float *partition = (float *) calloc(numbersPerProccess, sizeof(float));
+  readArrayAndSplitData(nProcess, myRank, length, numbersPerProccess, partition);  
 
+  // printf("partition %d\n", myRank);
+  // for(i = 0; i < numbersPerProccess; i++) {
+  //   printf("%f ", partition[i]);
+  // }
+  // printf("\n");
+
+  // Finalize MPI environment
   MPI_Finalize();
 	return 0;
 }
